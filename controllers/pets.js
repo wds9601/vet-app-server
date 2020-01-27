8a926f1974440ef19b26944b583aad5959f6ce14
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-01-27 10:25:56 -0800

    routes added to pets controller

---
+++ controllers/pets.js
@@ -0,0 +1,11 @@
+//TODO: GET all pets '/' assoc with one user
+
+//TODO: GET '/new' form for adding new pet to a user
+
+//TODO: GET '/:id' to view single pet by id
+
+//TODO: PUT '/:id' to edit pet data for one pet
+
+//TODO: POST '/' create new pet from form (include image)
+
+//TODO: DELETE '/:id' delete a pet from a user's pet list
