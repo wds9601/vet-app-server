758de1351f471b3cb1d08c3ed65b403232023514
bjgoddard <bjgoddard94@gmail.com>
2020-01-29 09:40:03 -0800

    BACK END ROUTING MAXIMUM EFFICIENCY

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,18 +1,14 @@
  
-// Add modules and dependencies
-const mongoose = require ('mongoose')
-const express = require('express')
+// // Add modules and dependencies
+// const mongoose = require ('mongoose')
+// const express = require('express')
 
 let router = require('express').Router()
 let db = require('../models')
 
 // // PETS
 //GET all pets '/' assoc with one user 
-
-//WORKS-TUESDAY
 router.get('/', (req, res) => {
-    // res.send('GET all pets from a user')
-    // console.log('Line 12', req.user.id, req.user._id)
     db.User.findById(req.user._id)
     .then((user) => {
         console.log('Line 15', user)
@@ -26,6 +22,39 @@ router.get('/', (req, res) => {
     .catch(err => {
         console.log('Error in GET ALL Pets route', err)
         res.status(503)
+    })
+})
+
+//POST '/' create new pet from form (include image)
+router.post('/', (req, res) => {
+    console.log(req.user)
+    db.User.findById(req.user._id)
+        .then(User => {
+            console.log(`This is the user`, User)
+            console.log(`And here are his pets`, User.pets)
+            User.pets.push({
+                name: req.body.name,
+                typeOfAnimal: req.body.typeOfAnimal,
+                breed: req.body.breed,
+                age: req.body.age,
+                sex: req.body.sex,
+                petImage: req.body.image,
+                summary: {
+                    rabiesShot: req.body.rabiesShot,
+                    microchip: req.body.microchip
+                },
+                treatment: {
+                    treatment: req.body.treatment,
+                    treatmentDate: req.body.treatmentDate
+                }
+            })
+            User.save()
+        .then(() => {
+            res.status(200).send({pets: User.pets})
+        })
+        .catch(err => {
+            console.log(err, 'Error')
+        })
     })
 })
 
@@ -69,39 +98,6 @@ router.put('/:id', (req, res) => {
     })
 })
 
-//POST '/' create new pet from form (include image)
-router.post('/', (req, res) => {
-    console.log(req.user)
-    db.User.findById(req.user._id)
-        .then(User => {
-            console.log(`This is the user`, User)
-            console.log(`And here are his pets`, User.pets)
-            User.pets.push({
-                name: req.body.name,
-                typeOfAnimal: req.body.typeOfAnimal,
-                breed: req.body.breed,
-                age: req.body.age,
-                sex: req.body.sex,
-                petImage: req.body.image,
-                summary: {
-                    rabiesShot: req.body.rabiesShot,
-                    microchip: req.body.microchip
-                },
-                treatment: {
-                    treatment: req.body.treatment,
-                    treatmentDate: req.body.treatmentDate
-                }
-            })
-            User.save()
-        .then(() => {
-            res.status(200).send({pets: User.pets})
-        })
-        .catch(err => {
-            console.log(err, 'Error')
-        })
-    })
-})
-
 
 //DELETE '/:id' delete a pet from a user's pet list
 router.delete('/:id', (req, res) => {
@@ -127,60 +123,126 @@ router.delete('/:id', (req, res) => {
 // GET - All medical records for single pet
 router.get('/:id/medical', (req, res) => {
     db.User.findById(req.user._id)
-    .then(User => {
-        let petSummary = User.pets.summary
-        console.log({pets: User.pets.summary})
-        res.send(petSummary)
+    .then(user => {
+        let thisPet = null;
+        for (let i = 0; i < user.pets.length; i++) {
+            if (user.pets[i]._id == req.params.id) {
+                thisPet = user.pets[i]
+            }
+        }
+        res.send({summary: thisPet.summary})
     })
     .catch(err => {
         console.log('error', err)
-        res.render('error')
+    })
+})
+router.put('/:id/medical', (req, res) => {
+    db.User.findById(req.user._id)
+    .then(User => {
+        let thisPet = null
+        for (let i = 0; i< User.pets.length; i++) {
+            if (User.pets[i]._id == req.params.id) {
+                thisPet = User.pets[i]
+            }
+        }
+        console.log("LINE 152", thisPet)
+        thisPet.summary.rabiesShot = req.body.rabiesShot,
+        thisPet.summary.microchip = req.body.microchip
+        User.save()
+        .then(() => {
+            res.status(200).send({summary: thisPet.summary })
+        })
+        .catch((err) => {
+            console.log('Error in PUT medical', err)
+        })
     })
 })
 
 
+router.post('/:id/medical', (req, res) => {
+    db.User.findById(req.user._id)
+    .then(User => {
+        let thisPet = null
+        for (let i = 0; i < User.pets.length; i++) {
+            if (User.pets[i]._id == req.params.id) {
+                thisPet = User.pets[i]
+            }
+        }
+        thisPet.summary.rabiesShot = req.body.rabiesShot
+        thisPet.summary.microchip = req.body.microchip
+        User.save()
+        .then(() => {
+            res.status(200).send({summary: thisPet.summary})
+        })
+        .catch((err) => {
+            console.log('error in POST medical', err)
+        })
+    })
+})
+
 // // TREATMENT ROUTES
-// GET - All details of single treatment
+// GET - All treatment details for a single pet
 router.get('/:id/treatment', (req, res) => {
     db.User.findById(req.user._id)
-    .then(treatment => {
-        console.log(User.pet.treatment)
-        res.send(treatment)
+    .then(user => {
+        let thisPet = null
+        for (let i = 0; i < user.pets.length; i++) {
+            if (user.pets[i]._id == req.params.id) {
+                thisPet = user.pets[i]
+            }
+        }
+        
+        res.send({treatment: thisPet.treatment})
+    })
+    .catch((err) => {
+        console.log('Error in GET treatment', err)
     })
 })
 
 // POST - create new pet treatment record
 router.post('/:id/treatment', (req, res) => {
-    db.User.create({
-        treatment: req.body.user.pet.treatment,
-        treatmentDate: req.body.user.pet.treatmentDate
-        .then(treatment => {
-            console.log(user.pet.treatment)
-            res.redirect('/:id/treatment')
+    db.User.findById(req.user._id)
+    .then(User => {
+        let thisPet = null
+        for (let i = 0; i < User.pets.length; i++) {
+            if (User.pets[i]._id == req.params.id) {
+                thisPet = User.pets[i]
+            }
+        }
+        thisPet.treatment.treatment = req.body.treatment,
+        thisPet.treatment.treatmentDate = req.body.treatmentDate
+
+        User.save()
+        .then(() => {
+            res.status(200).send({pet: User.pets })
         })
-        .catch(err => {
-            console.log('error', err)
-            res.render('error')
+        .catch((err) => {
+            console.log('error in POST treatment', err)
         })
     })
 })
 
 // PUT - edit a pet treatment record that already exists
-router.put('/:id/treatment', (req, res)=> {
-    db.User.findOneAndUpdate({
-        _id: req.params.id
-    },
-        req.body,
-    {
-        new: true
-    })
-    .then(updatedTreatment => {
-        res.redirect('/:id/treatment')
-    })
-    .catch(err => {
-        console.log('error', err)
-        res.render('error')
-    })
+router.put('/:id/treatment', (req, res) => {
+    db.User.findById(req.user._id)
+    .then(User => {
+        let thisPet = null
+        for (let i = 0; i < user.pets.length; i++) {
+            if (user.pets[i]._id == req.params.id) {
+                thisPet = user.pets[i]
+            }
+        }
+        thisPet.treatment.treatment = req.body.treatment,
+        thisPet.treatment.treatmentDate = req.body.treatmentDate
+
+        User.save()
+        .then(() => {
+            res.status(200).send({ pet: User.pets })
+        })
+        .catch((err) => {
+            console.log('error in PUT treatment', err)
+        })
+    })  
 })
 
 
